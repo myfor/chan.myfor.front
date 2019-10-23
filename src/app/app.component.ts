0341598dfec8e2ace9dd92a9371f8339060a4b20
mrunowen <birenchens@outlook.com>
2019-10-23 13:39:33 +0800

    update: set interval

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,28 @@ import { interval } from 'rxjs';
 })
 export class AppComponent {
   
+  ALLOW_WORDS_COUNT = 1024;
+  remainWrodCount = this.ALLOW_WORDS_COUNT;
   isDisabled = false;
+  
+  //  提交
   submitNewMessage(): void {
     this.isDisabled = true;
+
+    //  提交给服务器, 成功后计时提交的间隔
+
+
+    //  提交间隔
+    const SUBMIT_INTERVAL = 3000;
+     
+    let timer = setInterval(() => {
+      this.isDisabled = false;
+      alert('000')
+      clearInterval(timer);
+    }, SUBMIT_INTERVAL)
+  }
+
+  input(value: string) {
+    this.remainWrodCount = this.ALLOW_WORDS_COUNT - value.length;
   }
 }
--- src/app/directive/disabled.directive.ts
+++ src/app/directive/disabled.directive.ts
@@ -1,7 +1,7 @@
 import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
-  selector: '[appMfDisabled]'
+  selector: '[appDisabled]'
 })
 export class DisabledDirective {
 
